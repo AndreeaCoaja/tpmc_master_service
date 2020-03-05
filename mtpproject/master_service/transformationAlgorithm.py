3bf8c2a70b71ff0aff30a8d002d93600e156021c
AndreeaCoaja <53050097+AndreeaCoaja@users.noreply.github.com>
2020-03-05 17:54:46 +0200

    TransformationAlg

---
+++ mtpproject/master_service/transformationAlgorithm.py
@@ -0,0 +1,15 @@
+#this method defines what to be done for each type of phase (condition, operations)
+def transform(routine):
+    for phase in routine.phases:
+        if phase.type == "operations":
+            for id in phase.ComponentsId:
+                for component in routine.components:
+                    if component.id == id:
+                        component.execute()
+        else:
+            if phase.type == "condition":
+                for id in phase.ComponentsId:
+                    for component in routine.components:
+                        if component.id == id:
+                            if not component.activate():
+                                break
